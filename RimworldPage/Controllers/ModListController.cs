719524e360663891889e51616435fd873dc8bb40
Jacob Rivera <edjacob25@gmail.com>
2020-07-08 17:15:27 -0500

    Added files to show a modlist

--- RimworldPage/Controllers/ModListController.cs
+++ RimworldPage/Controllers/ModListController.cs
@@ -1,23 +1,33 @@
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using WebApplication.Models.Database;
 
 namespace WebApplication.Controllers
 {
     public class ModListController : Controller
     {
         private readonly ILogger<ModListController> _logger;
+        private readonly MyContext _context;
 
-        public ModListController(ILogger<ModListController> logger)
+        public ModListController(ILogger<ModListController> logger, MyContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         [HttpGet("/ModList/{id}")]
-        public IActionResult ShowList([FromRoute] string id)
+        public async Task<IActionResult> ShowList([FromRoute] string id)
         {
             _logger.LogDebug($"The id is {id}");
-            ViewBag.id = id;
-            return View();
+            var modList = await _context.ModLists
+                .Include(m => m.Mods)
+                .ThenInclude(m => m.Mod)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (modList == null) return NotFound();
+
+            return View(modList);
         }
     }
 }
