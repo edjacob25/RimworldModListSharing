7ba8e6fbba21018c28ddafa6cc832ef16c7e19eb
Jacob Rivera <edjacob25@gmail.com>
2020-07-08 19:31:15 -0500

    User-facing improvements

--- RimworldPage/Controllers/HomeController.cs
+++ RimworldPage/Controllers/HomeController.cs
@@ -104,11 +104,6 @@ namespace WebApplication.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
