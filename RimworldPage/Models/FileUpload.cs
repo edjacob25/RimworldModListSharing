ad5f516aa802c14eeab69345e80b457afb1cf233
Jacob Rivera <edjacob25@gmail.com>
2020-07-16 16:57:52 -0500

    Added version and expansions to the frontend

--- RimworldPage/Models/FileUpload.cs
+++ RimworldPage/Models/FileUpload.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -6,6 +7,9 @@ namespace WebApplication.Models
 {
     public class FileUpload
     {
+        public static readonly string[] Versions = { "1.0", "1.1" };
+        public static readonly string[] Expansions = { "Royalty" };
+
         [Required]
         [Display(Name = "File")]
         [BindProperty]
@@ -14,5 +18,13 @@ namespace WebApplication.Models
         [Display(Name = "Name for the list (Optional)")]
         [BindProperty]
         public string? Name { get; set; }
+
+        [Display(Name = "Rimworld version (Optional)")]
+        [BindProperty]
+        public string? Version { get; set; }
+
+        [Display(Name = "Expansions used (Optional)")]
+        [BindProperty]
+        public List<string>? ExpansionsUsed { get; set; }
     }
 }
